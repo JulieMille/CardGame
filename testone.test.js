30bda7278458534bdd735e9398f5bcfc8eb69cf7
JulieMille <millesunflower@mail.ru>
2023-08-09 16:33:07 +0300

    all done

---
+++ testone.test.js
@@ -0,0 +1,65 @@
+// const { it, expect } = require("@jest/globals");
+import { it, expect } from "@jest/globals";
+
+function game() {
+    this.cards = [];
+}
+
+const cardSuits = ["Diamonds", "Hearts", "Clubs", "Spades"];
+const cardRanks = ["6", "7", "8", "9", "10", "Q", "K", "J", "A"];
+
+function getRandomCard(cardsNum) {
+    return Math.floor(Math.random() * cardsNum);
+}
+
+function chooseLevel(cardsNum, array) {
+    for (let i = 0; i <= cardsNum - 1; i += 2) {
+        array[i] = [
+            cardSuits[getRandomCard(cardSuits.length)],
+            cardRanks[getRandomCard(cardRanks.length)],
+        ];
+        if (i + 1 < cardsNum) {
+            array[i + 1] = array[i];
+        }
+    }
+}
+
+// Подготовка
+
+// Действие
+
+// Проверка
+
+it("should add 6 elements to object", () => {
+    const g = new game();
+
+    chooseLevel(6, g.cards);
+
+    if (g.cards.length === 0) {
+        throw Error("game.cards array equal to 0");
+    }
+
+    expect(g.cards).toHaveLength(6);
+});
+
+it("should add 12 elements to object", () => {
+    const g = new game();
+
+    chooseLevel(12, g.cards);
+
+    if (g.cards.length === 0) {
+        throw Error("game.cards array equal to 0");
+    }
+    expect(g.cards).toHaveLength(12);
+});
+
+it("should add 18 elements to object", () => {
+    const g = new game();
+
+    chooseLevel(18, g.cards);
+
+    if (g.cards.length === 0) {
+        throw Error("game.cards array equal to 0");
+    }
+    expect(g.cards).toHaveLength(18);
+});
